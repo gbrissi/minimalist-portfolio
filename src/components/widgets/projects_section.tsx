0adf7c52217c767c51108f86871e131a4703cd9c
Gabriel Rissi <gabrielrissisc@gmail.com>
2024-02-23 10:49:22 -0300

    update project card ui

--- src/components/widgets/projects_section.tsx
+++ src/components/widgets/projects_section.tsx
@@ -1,4 +1,4 @@
-import { Card, Heading, Text } from "@radix-ui/themes";
+import { Box, Heading, Text } from "@radix-ui/themes";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 export default function ProjectsSection() {
@@ -61,9 +61,9 @@ function Project(props: ProjectProps) {
   const { t } = useTranslation(["translation"]);
 
   return (
-    <Card
+    <Box
       onClick={props.onClick}
-      className="p-0 m-0 flex cursor-pointer transition-colors dark:hover:bg-slate-900 hover:bg-slate-100"
+      className="p-0 flex-col m-0 flex cursor-pointer transition-colors dark:hover:bg-slate-950 hover:bg-slate-50"
     >
       <div className="flex bg-slate-50 dark:bg-slate-950 justify-center items-center w-full h-64 mb-4">
         {/* <ReloadIcon className="mr-2 h-4 w-4 animate-spin" /> */}
@@ -85,6 +85,6 @@ function Project(props: ProjectProps) {
           </Text>
         </div>
       </div>
-    </Card>
+    </Box>
   );
 }
